0b8556d566cd85a9f4b043631fc2df6a719bfb3c
danieltejano <53635919+danieltejano@users.noreply.github.com>
2023-01-20 21:25:30 +0800

    Created Routes Dictionary for the Project

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,3 +3,25 @@
  * Author: github.com/danieltejano
  * 
  */
+
+import DefaultLayout from '@/layout/default.vue'
+import HomePage from '@/pages/HomePage.vue'
+
+const routes = [
+    {
+        path: '/',
+        component: DefaultLayout,
+        children: [
+            {
+                name: 'home',
+                path: "",
+                component: HomePage,
+                meta: {
+                    title: import.meta.env.VITE_APP_NAME
+                }
+            }   
+        ]
+    }
+]
+
+export default routes;
