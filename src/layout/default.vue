91d85e0a19e6901df45ea34fa177d2480ceb25d6
danieltejano <53635919+danieltejano@users.noreply.github.com>
2023-01-20 21:28:52 +0800

    Created Default Layout

---
+++ src/layout/default.vue
@@ -0,0 +1,17 @@
+<template>
+    <router-view></router-view>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'DefaultLayout',
+    setup(){},
+    data(){
+        return{
+            componentOptions: {}
+        }
+    }
+})
+</script>
